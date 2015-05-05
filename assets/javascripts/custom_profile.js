828ab1a0be8aa11e4a151ce81e95510ecc27620e
Peter Lejeck <peter.lejeck@gmail.com>
2015-05-04 23:00:07 -0700

    Redesign user profile template

--- assets/javascripts/custom_profile.js
+++ assets/javascripts/custom_profile.js
@@ -1,19 +1,22 @@
 Discourse.UserRoute.reopen({
   afterModel: function() {
     user = this.modelFor('user');
-    user.setProperties({"hummingbird": {"loaded": false}});
-    $.ajax("https://hummingbird.me/api/v1/users/" + user.get('username'), {
+    user.setProperties({hummingbird: {loaded: false}});
+    $.ajax("https://hummingbird.me/users/" + user.get('username'), {
       type: "GET",
       contentType: "application/json; charset=utf-8",
       success: function(data) {
+        var hb = data.user;
         user.setProperties({
-          "hummingbird": {
-            "loaded": true,
-            "coverImage": data.cover_image,
-            "online": data.online,
-            "following": data.following
+          hummingbird: {
+            loaded: true,
+            coverImageStyle: ('background-image: url("'+hb.cover_image_url+'")').htmlSafe(),
+            followingCount: hb.following_count,
+            follwerCount: hb.follower_count,
+            isPro: hb.is_pro,
+            bio: hb.bio
           }
-        })
+        });
       },
       xhrFields: {
         withCredentials: true

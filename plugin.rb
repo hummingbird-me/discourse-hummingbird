1b5763eff060cddcc6210123604d8547e0c3fc88
Peter Lejeck <peter.lejeck@gmail.com>
2016-04-06 16:51:01 -0700

    Force our pro_expires_at custom field to show

--- plugin.rb
+++ plugin.rb
@@ -2,3 +2,10 @@
 # about: discourse customizations for hummingbird
 # version: 0.1
 # authors: Hummingbird Media
+
+require_dependency 'user'
+class ::User
+  def self.whitelisted_user_custom_fields(guardian)
+    (super(guardian) + 'pro_expires_at').uniq
+  end
+end

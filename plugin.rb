2853333f98009c75f19b35f0c1a3a9927e48dd8d
Peter Lejeck <peter.lejeck@gmail.com>
2016-04-06 17:10:03 -0700

    Try evaluating after initialize

--- plugin.rb
+++ plugin.rb
@@ -3,8 +3,10 @@
 # version: 0.1
 # authors: Hummingbird Media
 
-User.class_eval do
-  def self.whitelisted_user_custom_fields(guardian)
-    (super(guardian) + 'pro_expires_at').uniq
+after_initialize do
+  User.class_eval do
+    def self.whitelisted_user_custom_fields(guardian)
+      (super(guardian) + 'pro_expires_at').uniq
+    end
   end
 end

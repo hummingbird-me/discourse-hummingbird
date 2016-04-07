e9f325e447ead5be2c36d2bba4bbb70d71145fa5
Peter Lejeck <peter.lejeck@gmail.com>
2016-04-06 17:03:36 -0700

    Make use of class_eval instead of reopening

--- plugin.rb
+++ plugin.rb
@@ -3,7 +3,7 @@
 # version: 0.1
 # authors: Hummingbird Media
 
-class ::User
+User.class_eval do
   def self.whitelisted_user_custom_fields(guardian)
     (super(guardian) + 'pro_expires_at').uniq
   end

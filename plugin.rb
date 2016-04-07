accc63fba6890088e9147ddd314a1f1d73be1143
Peter Lejeck <peter.lejeck@gmail.com>
2016-04-06 18:50:13 -0700

    Ditch whitelist shit for now

--- plugin.rb
+++ plugin.rb
@@ -2,11 +2,3 @@
 # about: discourse customizations for hummingbird
 # version: 0.1
 # authors: Hummingbird Media
-
-after_initialize do
-  User.class_eval do
-    def self.whitelisted_user_custom_fields(guardian)
-      (super(guardian) + ['pro_expires_at']).uniq
-    end
-  end
-end
